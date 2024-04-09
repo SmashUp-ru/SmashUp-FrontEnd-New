a5a7b819b793432759588d3eefba6634dabc80b0
dmhd6210 <svyatoslavsvyatkin@yandex.ru>
2024-04-09 14:55:24 +0300

    Add play button to card component

--- src/components/main/Card.tsx
+++ src/components/main/Card.tsx
@@ -1,22 +1,44 @@
-import React from 'react';
+'use client';
+
+import { useState } from 'react';
 import Image, { StaticImageData } from 'next/image';
+import PlayIcon from '@/components/icons/PlayIcon';
+import PauseIcon from '@/components/icons/PauseIcon';
 
 export default function Card({
+    id,
     title,
     description,
     image
 }: {
+    id: number;
     title: string;
     description: string;
     image: string | StaticImageData;
 }) {
+    const [isPlaying, setIsPlaying] = useState(false);
+
     return (
         <div className='flex flex-col items-center gap-3 bg-sidebar-gray min-w-[238px] h-[301px] rounded-4xl px-6 py-6'>
-            <Image src={image} alt={title} className='w-full' />
-            <div className='flex flex-col gap-2 text-left w-full'>
+            <div className='w-full relative group'>
+                <Image src={image} alt={title} className='w-full ' />
+                <button
+                    className='hidden group-hover:block absolute right-2.5 bottom-2.5'
+                    onClick={() => {
+                        setIsPlaying(!isPlaying);
+                    }}
+                >
+                    {isPlaying ? (
+                        <PauseIcon width={48} height={48} color='primary' />
+                    ) : (
+                        <PlayIcon width={48} height={48} color='primary' />
+                    )}
+                </button>
+            </div>
+            <a className='flex flex-col gap-2 text-left w-full' href={`/playlist/${id}`}>
                 <span className='text-secondary-text font-semibold text-base'>{title}</span>
                 <span className='text-neutral-400 font-normal text-base '>{description}</span>
-            </div>
+            </a>
         </div>
     );
 }
