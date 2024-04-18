433ee7e269b1bc89c9a205a32da7a854dabf3f33
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2024-04-18 14:22:29 +0300

    Updated to Figma: Search Page

--- src/app/(root)/search/page.tsx
+++ src/app/(root)/search/page.tsx
@@ -12,12 +12,10 @@ export default function Search() {
             {/* Поиск */}
             <div className='flex flex-row space-betwen gap-8'>
                 {/* Лучший результат */}
-                <div className='w-1/2'>
-                    <h2 className='font-semibold text-2xl text-gray-header pb-5'>
-                        Лучший результат
-                    </h2>
+                <div className='w-1/2 flex flex-col gap-5'>
+                    <h2 className='font-semibold text-2xl text-gray-header'>Лучший результат</h2>
 
-                    <div className='flex flex-row bg-sidebar-gray w-full max-w-[790px] h-[238px] rounded-4xl px-6 py-6 gap-12'>
+                    <div className='flex flex-row gap-12 bg-sidebar-gray w-full max-w-[790px] h-[238px] rounded-4xl px-6 py-6 '>
                         <Image
                             src={profile}
                             width={188}
@@ -28,23 +26,21 @@ export default function Search() {
 
                         <div className='flex flex-col justify-center gap-2.5'>
                             <h1 className='font-semibold text-5xl text-secondary-text'>dmhd6219</h1>
-                            <div className='flex gap-2'>
-                                <span className='font-medium text-base text-gray-400'>
+                            <div className='flex gap-2.5'>
+                                <span className='font-medium text-base text-icon'>
                                     500 подписчиков
                                 </span>
-                                <Separator className='font-medium text-base text-gray-400' />
-                                <span className='font-medium text-base text-gray-400'>
-                                    115 мэшапов
-                                </span>
+                                <Separator className='font-medium text-base text-icon' />
+                                <span className='font-medium text-base text-icon'>115 мэшапов</span>
                             </div>
                         </div>
                     </div>
                 </div>
 
                 {/* Мэшапы */}
-                <div className='w-1/2'>
-                    <div className='w-full flex flex-row justify-between'>
-                        <h2 className='font-semibold text-2xl text-gray-header pb-5'>Мэшапы</h2>
+                <div className='w-1/2 flex flex-col gap-5'>
+                    <div className='w-full flex flex-row justify-between '>
+                        <h2 className='font-semibold text-2xl text-gray-header'>Мэшапы</h2>
                         <span className='font-bold text-sm text-gray-300 uppercase'>
                             Показать всё
                         </span>
@@ -59,14 +55,12 @@ export default function Search() {
             </div>
 
             {/* Недавно прослушано */}
-            <div>
+            <div className='flex flex-col gap-5'>
                 <div className='flex flex-row justify-between'>
-                    <h2 className='font-semibold text-2xl text-gray-header pb-5'>
-                        Недавно прослушано
-                    </h2>
+                    <h2 className='font-semibold text-2xl text-gray-header'>Недавно прослушано</h2>
                     <span className='font-bold text-sm text-gray-300 uppercase'>Показать всё</span>
                 </div>
-                <div className='w-full h-[301px] flex-wrap overflow-hidden flex flex-row gap-7'>
+                <div className='w-full flex flex-row gap-7'>
                     {search.map((item) => (
                         <Card key={item.id} {...item} />
                     ))}
