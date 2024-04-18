a2afcf362bcb9791d25aa9d382d7a44b435d037c
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2024-04-18 14:26:39 +0300

    Updated to Figma: Profile Page

--- src/app/(root)/profile/dmhd6219/page.tsx
+++ src/app/(root)/profile/dmhd6219/page.tsx
@@ -13,7 +13,7 @@ import TrackItem from '@/components/TrackItem';
 
 export default function Profile() {
     return (
-        <div className='px-8'>
+        <div className='px-8 flex flex-col gap-5'>
             {/* Профиль */}
             <div className='flex flex-row bg-sidebar-gray w-full h-[238px] rounded-4xl px-6 py-6 gap-12'>
                 <Image
@@ -25,11 +25,11 @@ export default function Profile() {
                 />
 
                 <div className='flex flex-col justify-center gap-2.5'>
-                    <div className='flex items-center gap-5'>
+                    <div className='flex items-center gap-4'>
                         <h1 className='font-semibold text-5xl text-secondary-text'>dmhd6219</h1>
                         <DoneIcon width={34} height={24} />
                     </div>
-                    <div className='flex gap-2'>
+                    <div className='flex gap-2.5'>
                         <span className='font-medium text-base text-icon'>5 Мэшапов</span>
                         <Separator className='font-medium text-base text-icon' />
                         <span className='font-medium text-base text-icon'>666 подписчиков</span>
@@ -39,24 +39,28 @@ export default function Profile() {
                 </div>
             </div>
 
-            {/* Действия */}
-            <div className='flex flex-row gap-5 items-center mt-5 mb-7'>
-                <PlayIcon width={48} height={48} color='primary' />
-                <AllCategoriesIcon width={26} height={26} color='icon' />
-                <SettingsIcon width={28} height={28} color='icon' />
-                <ShareIcon width={26} height={22} color='icon' />
-                <AddIcon width={26} height={26} color='icon' />
-            </div>
-
-            {/* Семые популярные мэшапы */}
-            <div className='flex flex-col gap-4'>
-                <h2 className='font-semibold text-2xl'>Самые популярные мэшапы</h2>
-                <div className='flex flex-col'>
-                    {profile_popular.map((item) => (
-                        <TrackItem key={item.id} {...item} />
-                    ))}
+            <div className='flex flex-col gap-8'>
+                {/* Действия */}
+                <div className='flex flex-row gap-5 items-center'>
+                    <PlayIcon width={48} height={48} color='primary' />
+                    <AllCategoriesIcon width={26} height={26} color='icon' />
+                    <SettingsIcon width={28} height={28} color='icon' />
+                    <ShareIcon width={26} height={22} color='icon' />
+                    <AddIcon width={26} height={26} color='icon' />
                 </div>
-                <span className='font-bold text-sm text-gray-300 uppercase'>Все</span>
+
+                {/* Семые популярные мэшапы */}
+                <div className='flex flex-col gap-4'>
+                    <h2 className='font-semibold text-2xl'>Самые популярные мэшапы</h2>
+                    <div className='flex flex-col'>
+                        {profile_popular.map((item) => (
+                            <TrackItem key={item.id} {...item} />
+                        ))}
+                    </div>
+                    <span className='font-bold text-sm text-gray-300 uppercase cursor-pointer'>
+                        Все
+                    </span>
+                </div>
             </div>
         </div>
     );
