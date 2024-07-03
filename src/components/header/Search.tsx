bf42ace275184932c6c5f193253c5215235edf72
dmhd6219 <s.sviatkin@innopolis.university>
2024-07-03 12:50:44 +0300

    feat: add popover to crossover search

--- src/components/header/Search.tsx
+++ src/components/header/Search.tsx
@@ -4,13 +4,21 @@ import SearchIcon from '@/components/icons/SearchIcon';
 import SmashUpInput from '@/components/smashup/Input/Input';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { useTranslations } from 'next-intl';
+import { Popover } from 'react-tiny-popover';
+import Image from 'next/image';
+import egor from '/public/dev/search/egor.png';
+import SearchContext from '@/providers/search';
 
 export default function Search() {
     const pathname = usePathname();
     const router = useRouter();
     const searchParams = useSearchParams();
+
+    const { type } = useContext(SearchContext);
+
+    const searchRef = useRef<HTMLInputElement | null>(null);
 
     const [searchValue, setSearchValue] = useState(
         searchParams.get('q') ? searchParams.get('q')?.toString() : ''
@@ -33,21 +41,84 @@ export default function Search() {
 
     const t = useTranslations('components.header.search');
 
+    const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+
     return (
-        <div onClick={() => {}} className='py-[1px] flex-grow'>
-            <SmashUpInput
-                placeholder={t('search')}
-                icon={<SearchIcon width={16} height={16} color='onSurface' />}
-                className=''
+        <Popover
+            isOpen={isPopoverOpen}
+            positions={['bottom']} // preferred positions by priority
+            content={
+                <div
+                    className={`bg-surface rounded-lg flex flex-col gap-2.5`}
+                    style={{ width: searchRef.current?.clientWidth }}
+                >
+                    <a className='p-5 font-medium text-base' href='/search'>
+                        Все результаты
+                    </a>
+                    <div>
+                        {/*Мэшаперы*/}
+                        <div className='px-5'>
+                            <span className='font-semibold text-base text-white py-2.5'>
+                                Мэшаперы
+                            </span>
+                            <div className='flex flex-row items-center gap-4 py-2.5'>
+                                <Image
+                                    src={egor}
+                                    alt='Иконка профиля'
+                                    className='w-8 h-8 rounded'
+                                />
+                                <span className='font-medium text-base text-icon'>dmhd6219</span>
+                            </div>
+                        </div>
+
+                        {/*Мэшапы*/}
+                        <div className='px-5'>
+                            <span className='font-semibold text-base text-white py-2.5'>
+                                Мэшапы
+                            </span>
+                            <div className='flex flex-row items-center gap-4 py-2.5'>
+                                <Image src={egor} alt='Иконка мэшапа' className='w-8 h-8 rounded' />
+                                <span className='font-medium text-base text-icon'>ДОРАДУЛО</span>
+                            </div>
+                        </div>
+
+                        {/*Треки*/}
+                        <div className='px-5'>
+                            <span className='font-semibold text-base text-white py-2.5'>Треки</span>
+                            <div className='flex flex-row items-center gap-4 py-2.5'>
+                                <Image src={egor} alt='Иконка трека' className='w-8 h-8 rounded' />
+                                <span className='font-medium text-base text-icon'>Дора Дура</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            }
+            onClickOutside={() => setIsPopoverOpen(false)}
+            padding={0}
+        >
+            <div
                 onClick={() => {
-                    router.push(`/search${searchValue ? `?q=${searchValue}` : ''}`);
+                    if (type === 'crossover') {
+                        setIsPopoverOpen(true);
+                    }
                 }}
-                onChange={(e) => {
-                    setSearchValue(e.target.value);
-                    handleSearch(e.target.value);
-                }}
-                value={searchValue}
-            />
-        </div>
+                className='py-[1px] flex-grow'
+            >
+                <SmashUpInput
+                    ref={searchRef}
+                    placeholder={t('search')}
+                    icon={<SearchIcon width={16} height={16} color='onSurface' />}
+                    className=''
+                    onClick={() => {
+                        router.push(`/search${searchValue ? `?q=${searchValue}` : ''}`);
+                    }}
+                    onChange={(e) => {
+                        setSearchValue(e.target.value);
+                        handleSearch(e.target.value);
+                    }}
+                    value={searchValue}
+                />
+            </div>
+        </Popover>
     );
 }
--- src/components/smashup/Input/Input.tsx
+++ src/components/smashup/Input/Input.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { forwardRef, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
@@ -13,54 +13,66 @@ export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> 
     defaultValue?: string;
 }
 
-export default function SmashUpInput({
-    className,
-    isFocused,
-    isError,
-    helper,
-    icon,
-    heading,
-    value,
-    defaultValue,
-    onChange,
-    ...props
-}: InputProps) {
-    const [focus, setFocus] = useState<boolean>(isFocused ? isFocused : false);
-    const [error] = useState<boolean>(isError ? isError : false);
+const SmashUpInput = forwardRef(
+    (
+        {
+            className,
+            isFocused,
+            isError,
+            helper,
+            icon,
+            heading,
+            value,
+            defaultValue,
+            onChange,
+            ...props
+        }: InputProps,
+        ref: React.ForwardedRef<HTMLInputElement | null>
+    ) => {
+        const [focus, setFocus] = useState<boolean>(isFocused ? isFocused : false);
+        const [error] = useState<boolean>(isError ? isError : false);
 
-    return (
-        <div className={twMerge('w-full flex flex-col gap-2.5', className)}>
-            {heading && (
-                <span className='font-medium text-base text-onSurfaceVariant text-left'>
-                    {heading}
-                </span>
-            )}
+        return (
+            <div className={twMerge('w-full flex flex-col gap-2.5', className)}>
+                {heading && (
+                    <span className='font-medium text-base text-onSurfaceVariant text-left'>
+                        {heading}
+                    </span>
+                )}
 
-            <div className='w-full flex justify-start items-center relative'>
-                <input
-                    onFocus={() => {
-                        if (isFocused === undefined) setFocus(true);
-                    }}
-                    onBlur={() => {
-                        if (isFocused === undefined) setFocus(false);
-                    }}
-                    className={twMerge(
-                        'w-full bg-surface py-[14px] pl-[25px] rounded-2xl',
-                        'font-normal text-base text-onSurfaceVariant placeholder-onSurfaceVariant',
-                        icon ? 'pl-14' : '',
-                        focus ? 'border-none outline outline-primary' : '',
-                        error ? 'border-none outline outline-red-600' : ''
-                    )}
-                    type='text'
-                    value={value}
-                    defaultValue={defaultValue}
-                    onChange={onChange}
-                    {...props}
-                />
-                <div className='absolute ml-5'>{icon}</div>
+                <div className='w-full flex justify-start items-center relative'>
+                    <input
+                        onFocus={() => {
+                            if (isFocused === undefined) setFocus(true);
+                        }}
+                        onBlur={() => {
+                            if (isFocused === undefined) setFocus(false);
+                        }}
+                        className={twMerge(
+                            'w-full bg-surface py-[14px] pl-[25px] rounded-2xl',
+                            'font-normal text-base text-onSurfaceVariant placeholder-onSurfaceVariant',
+                            icon ? 'pl-14' : '',
+                            focus ? 'border-none outline outline-primary' : '',
+                            error ? 'border-none outline outline-red-600' : ''
+                        )}
+                        type='text'
+                        value={value}
+                        defaultValue={defaultValue}
+                        onChange={onChange}
+                        ref={ref}
+                        {...props}
+                    />
+                    <div className='absolute ml-5'>{icon}</div>
+                </div>
+
+                {helper && (
+                    <span className='font-normal text-xs text-onSurfaceVariant'>{helper}</span>
+                )}
             </div>
+        );
+    }
+);
 
-            {helper && <span className='font-normal text-xs text-onSurfaceVariant'>{helper}</span>}
-        </div>
-    );
-}
+SmashUpInput.displayName = 'SmashUpInput';
+
+export default SmashUpInput;
